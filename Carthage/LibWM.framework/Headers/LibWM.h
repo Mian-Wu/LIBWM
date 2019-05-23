8df58790f898cb91716994985f75318aa9fd9f63
Mian-Wu <1256633846@qq.com>
2019-05-23 15:46:52 +0800

    提交

---
+++ Carthage/LibWM.framework/Headers/LibWM.h
@@ -0,0 +1,19 @@
+//
+//  LibWM.h
+//  LibWM
+//
+//  Created by 吴冕 on 2019/5/23.
+//  Copyright © 2019 wumian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibWM.
+FOUNDATION_EXPORT double LibWMVersionNumber;
+
+//! Project version string for LibWM.
+FOUNDATION_EXPORT const unsigned char LibWMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibWM/PublicHeader.h>
+
+
